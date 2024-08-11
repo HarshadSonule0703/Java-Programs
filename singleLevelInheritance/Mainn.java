8f9e35d71d3dd62a44335e75eae26c335be78b90
HarshadSonule0703 <harshadsonule44@gmail.com>
2024-08-11 21:12:21 +0530

    First Java Program

---
+++ singleLevelInheritance/Mainn.java
@@ -0,0 +1,19 @@
+class Animal{
+	int x = 1;
+	
+}
+class Dog extends Animal{
+	int y = x;
+	public void Details(){
+		System.out.println("y is :"+y +"   x is: "+x);
+	}
+
+}
+public class Mainn{
+public static void main(String[] args) {
+    Dog d1 = new Dog();
+    d1.Details();
+    
+    
+}
+}
